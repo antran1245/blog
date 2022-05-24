f3a2e8d27f4718448689b28f23d3dffdacfc0802
antran1245 <antran1245@gmail.com>
2022-05-23 22:25:54 -0700

    axios call to database

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import Post from "./Post";
 
 export default function Dashboard() {
     const [postListing, setPostListing]= useState([])
 
+    useEffect(() => {
+        const handleAxios = async() => {
+            const resp = await axios.get('http://localhost:8000/api/posts')
+            console.log(resp.data)
+            setPostListing(resp.data)
+        }
+        handleAxios()
+    }, [])
     return(
         <div>
             <Post/>
